f264d105ab414a8d22c8d3a8e091df3bc9f86d7f
Qh92 <771600967@qq.com>
2021-10-18 22:46:10 +0800

    feat:添加sentinel降级、熔断测试代码

--- cloudalibaba-sentinel-service8401/src/main/java/com/qinh/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/qinh/controller/FlowLimitController.java
@@ -1,5 +1,6 @@
 package com.qinh.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import java.util.concurrent.TimeUnit;
  * @date 2021-10-17 16:09
  */
 @RestController
+@Slf4j
 public class FlowLimitController {
     @GetMapping("/testA")
     public String testA() {
@@ -29,5 +31,26 @@ public class FlowLimitController {
 
         return "------testB";
     }
+
+    @GetMapping("/testD")
+    public String testD()
+    {
+        /*try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        log.info("testD 测试RT");*/
+
+        log.info("testD 测试RT");
+        int age = 10/0;
+
+        return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE()
+    {
+        log.info("testE 测试异常数");
+        int age = 10/0;
+        return "------testE 测试异常数";
+    }
+
 
 }
